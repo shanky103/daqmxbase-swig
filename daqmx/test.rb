d37453ca9c5ca5e1e93b4cb96eac56b8d4144ca6
bikenomad <bikenomad@f1de5cad-1c3e-0410-bf5e-775203a4c448>
2007-11-07 18:22:58 +0000

    test on mac

--- daqmx/test.rb
+++ daqmx/test.rb
@@ -1,19 +1,24 @@
 require "daqmxbase"
 
 def testOne
-  chan = "Dev1/ai0:2"
+  deviceName = "Dev1"
+  chan = deviceName + "/ai0:2"
   points_to_read = 3
   timeout = 10.0
   samples_per_chan = 10
+  minVal = -1.0
+  maxVal = 1.0
 
-  task = Daqmxbase::Task.new("")
-print "create_aivoltage_chan: "
-  p *task.create_aivoltage_chan("Dev1/ai0",Daqmxbase::VAL_CFG_DEFAULT,-1.0,1.0,Daqmxbase::VAL_VOLTS)
-print "start: "
-  p *task.start()
-print "read_analog_f64: " 
-  p *task.read_analog_f64(points_to_read, timeout, Daqmxbase::VAL_GROUP_BY_SCAN_NUMBER, samples_per_chan)
-puts "done"
+  Daqmxbase::reset_device(deviceName)
+  task = Daqmxbase::Task.new
+  print "create_aivoltage_chan: "
+  p task.create_aivoltage_chan(chan,Daqmxbase::VAL_CFG_DEFAULT,minVal,maxVal,Daqmxbase::VAL_VOLTS)
+  print "start: "
+  p task.start()
+  print "read_analog_f64: " 
+  (data, pointsRead) = task.read_analog_f64(points_to_read, timeout, Daqmxbase::VAL_GROUP_BY_SCAN_NUMBER, samples_per_chan)
+  p [data, pointsRead]
+  puts "done"
 end
 
 testOne()
