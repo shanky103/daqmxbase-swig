0b434d54aabc1dfac020fa1d977ea62fc9fbf593
bikenomad <bikenomad@f1de5cad-1c3e-0410-bf5e-775203a4c448>
2007-11-08 02:59:02 +0000

    made work with new build

--- daqmx/efield.rb
+++ daqmx/efield.rb
@@ -1,6 +1,8 @@
-require 'daqmx'
+require 'daqmxbase'
 
 class Efield
+  include Daqmxbase
+
   # electrode addresses
   INT_SRC = 0
   E1 = 1; E2 = 2; E3 = 3; E4 = 4; E5 = 5; E6 = 6; E7 = 7; E8 = 8; E9 = 9
@@ -22,28 +24,28 @@ class Efield
   end
 
   def createAITask
-    task = USB600x::Task.new()
-    task.create_aivoltage_chan(aiChanName, Daqmxbase::VAL_RSE, MIN_LEVEL, MAX_LEVEL, Daqmxbase::VAL_VOLTS)
+    task = Task.new()
+    task.create_aivoltage_chan(aiChanName, VAL_RSE, MIN_LEVEL, MAX_LEVEL, VAL_VOLTS)
     if SAMPLES_TO_AVERAGE > 1
-      task.cfg_samp_clk_timing("OnboardClock", SAMPLE_RATE, Daqmxbase::VAL_RISING, Daqmxbase::VAL_FINITE_SAMPS, SAMPLES_TO_AVERAGE)
+      task.cfg_samp_clk_timing("OnboardClock", SAMPLE_RATE, VAL_RISING, VAL_FINITE_SAMPS, SAMPLES_TO_AVERAGE)
     end
     task
   end
 
   def createDOTask
-    task = USB600x::Task.new()
+    task = Task.new()
     task.create_dochan(doChanName)
     task.start
     task
   end
 
   def readRawChannelLevel(channelAddress)
-    sampsWritten = @digitalOutputTask.write_digital_u8(1, 0, TIMEOUT, Daqmxbase::VAL_GROUP_BY_CHANNEL, channelAddress)
-    p [ sampsWritten, @digitalOutputTask.done? ]
+    sampsWritten = @digitalOutputTask.write_digital_u8(1, 0, TIMEOUT, VAL_GROUP_BY_CHANNEL, channelAddress)
+    p [ sampsWritten, @digitalOutputTask.is_task_done ]
     # read all the samples; wait till done
     @analogInputTask.start
-    (data, samplesPerChanRead) =  @analogInputTask.read_analog_f64(Daqmxbase::VAL_AUTO, TIMEOUT, Daqmxbase::VAL_GROUP_BY_CHANNEL, SAMPLES_TO_AVERAGE)
-    p [ data, samplesPerChanRead, @analogInputTask.done? ]
+    (data, samplesPerChanRead) =  @analogInputTask.read_analog_f64(VAL_AUTO, TIMEOUT, VAL_GROUP_BY_CHANNEL, SAMPLES_TO_AVERAGE)
+    p [ data, samplesPerChanRead, @analogInputTask.is_task_done ]
     @analogInputTask.stop
     if SAMPLES_TO_AVERAGE > 1
       return data.inject(0.0) { |s,i| s + i} / data.size
@@ -65,7 +67,7 @@ class Efield
     @selectOutputs = "port0/line0:3"
     @analogInputTask = createAITask()
     @digitalOutputTask = createDOTask()
-    Daqmxbase::reset_device(@devName)
+    reset_device(@devName)
   end
 
   def references
